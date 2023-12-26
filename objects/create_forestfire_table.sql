8f8261393bb5cddc1d01ec91f59c2f1de1562ad3
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-26 10:06:25 +0530

    Create create_forestfire_table.sql

---
+++ objects/create_forestfire_table.sql
@@ -0,0 +1,17 @@
+USE {{ params.schema_name }};
+CREATE OR REPLACE TRANSIENT TABLE {{ params.table_name }}
+        (
+            id INT,
+            y INT,
+            month VARCHAR(25),
+            day VARCHAR(25),
+            ffmc FLOAT,
+            dmc FLOAT,
+            dc FLOAT,
+            isi FLOAT,
+            temp FLOAT,
+            rh FLOAT,
+            wind FLOAT,
+            rain FLOAT,
+            area FLOAT
+        );
