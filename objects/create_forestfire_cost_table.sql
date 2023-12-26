e74b33be82a5ac03ca0de22e28e8fd5e0f4a0502
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-26 10:08:07 +0530

    Create create_forestfire_cost_table.sql

---
+++ objects/create_forestfire_cost_table.sql
@@ -0,0 +1,13 @@
+USE {{ params.schema_name }};
+CREATE OR REPLACE TRANSIENT TABLE {{ params.table_name }}
+        (
+            id INT,
+            land_damage_cost INT,
+            property_damage_cost INT,
+            lost_profits_cost INT,
+            total_cost INT,
+            y INT,
+            month VARCHAR(25),
+            day VARCHAR(25),
+            area FLOAT
+        );
