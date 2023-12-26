62dece0ed7a8c3e8f0b08c46c8bab53fe08eead7
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-26 23:18:40 +0530

    Update create_stored_procedure.sql

--- objects/create_stored_procedure.sql
+++ objects/create_stored_procedure.sql
@@ -1,16 +1,11 @@
 USE SCHEMA TEST_DEV_DB.TEST_SCHEMA;
 
-CREATE OR REPLACE PROCEDURE myproc(from_table string, to_table string, count int)
+CREATE OR REPLACE PROCEDURE myproc()
   RETURNS STRING
-  LANGUAGE PYTHON
-  RUNTIME_VERSION = '3.8'
-  packages = ('snowflake-snowpark-python')
-  HANDLER = 'run'
-as
-$$
-def run(session, from_table, to_table, count):
-  session.table(from_table).limit(count).write.save_as_table(to_table)
-  return "SUCCESS"
-$$;
+  LANGUAGE JAVASCRIPT
+  EXECUTE AS CALLER
+  AS '
+  return "Hello from the snowflake stored procedure!";
+  ';
 
-CALL myproc('my_table', 'table_b', 1);
+CALL myproc();
