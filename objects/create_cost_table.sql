0fd580945325db16d17f457bb2e93dc596d0ef2f
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-26 10:07:23 +0530

    Create create_cost_table.sql

---
+++ objects/create_cost_table.sql
@@ -0,0 +1,8 @@
+USE {{ params.schema_name }};
+CREATE OR REPLACE TRANSIENT TABLE {{ params.table_name }}
+        (
+            id INT,
+            land_damage_cost INT,
+            property_damage_cost INT,
+            lost_profits_cost INT
+        );
