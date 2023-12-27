bca45c2650d716353854648af0ccf2bca481f09e
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-27 13:56:53 +0530

    Update create_stream.sql

--- objects/create_stream.sql
+++ objects/create_stream.sql
@@ -1,7 +1,4 @@
 USE SCHEMA TEST_DEV_DB.TEST_SCHEMA;
 
 CREATE OR REPLACE STREAM my_stream ON TABLE my_table;
-SELECT * FROM my_stream;
-INSERT INTO my_table VALUES (3, 'Alice', 'Miller');
-SELECT * FROM my_stream;
 
