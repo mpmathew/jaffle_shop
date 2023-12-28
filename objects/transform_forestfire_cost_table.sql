0652fe3606770ad8aad73fe9d93b7e32ca65be86
Midhun <99331155+mpmathew@users.noreply.github.com>
2023-12-28 09:48:24 +0530

    Delete objects/transform_forestfire_cost_table.sql

--- objects/transform_forestfire_cost_table.sql
+++
@@ -1,9 +0,0 @@
-USE {{ params.schema_name }};
-SELECT
-        id,
-        month,
-        day,
-        total_cost,
-        area,
-        total_cost / area as cost_per_area
-    FROM {{ params.table_name }};
