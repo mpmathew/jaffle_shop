9f96553d09ea49e747808237f906e1338366b3f5
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-01-19 01:17:40 +0530

    Update log_variable.sql

--- macros/log_variable.sql
+++ macros/log_variable.sql
@@ -1,3 +1,3 @@
-{% macro log_variable(my_variable) %}
- {% do log("~ argument(my_variable): "  ~ env_var(my_variable), info=True) %}
+{% macro log_variable() %}
+ {% do log('DBT_ESG_DB: ' ~ env_var(DBT_ESG_DB), info=True) %}
 {% endmacro %}
