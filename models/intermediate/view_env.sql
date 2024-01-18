fea49675d5f83a1859a6bce564b68ddd380b64ce
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-01-19 00:36:06 +0530

    Update view_env.sql

--- models/intermediate/view_env.sql
+++ models/intermediate/view_env.sql
@@ -1,3 +1,6 @@
 
-SELECT 'DBT_ESG_DB' as variable, '{{env_var('DBT_ESG_DB')}}' as value
+--SELECT 'DBT_ESG_DB' as variable, '{{env_var('DBT_ESG_DB')}}' as value
+{{ log_variable() }}
+
+select 1 as dummy
     
