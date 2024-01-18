0be2fe24066156f941bd1bcdef32eaec83e981f9
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-01-19 00:12:07 +0530

    Update view_env.sql

--- models/intermediate/view_env.sql
+++ models/intermediate/view_env.sql
@@ -1,5 +1,3 @@
-env_vars_table as (
-SELECT 'DBT_ESG_DB' as variable, {{env_var('DBT_ESG_DB')}} as value UNION ALL
-)
-select * from env_vars_table
+
+SELECT 'DBT_ESG_DB' as variable, {{env_var('DBT_ESG_DB')}} as value
     
