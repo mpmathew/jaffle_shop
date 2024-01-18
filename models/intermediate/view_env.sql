aafa72abcc96e123f894ef2585ab2f84f017ae14
Midhun <99331155+mpmathew@users.noreply.github.com>
2024-01-18 22:32:19 +0530

    Update view_env.sql

--- models/intermediate/view_env.sql
+++ models/intermediate/view_env.sql
@@ -1,21 +1,10 @@
--- models/environment_variables_model.sql
-
-with source as (
-    select * from {{ ref('variables') }}
-
-),
-
-  -- Staging model to load the environment variable names into a temporary table
-
-environment_variable_names as (
-    select 
-    variable
-    from source
-)
-
 -- Model block to define the transformation and fetch values using env_var()
+with variables as (
 
     select 
     variable,
-    "{{env_var(variable)}}" as value
-    from environment_variable_names
+    "{{env_var('variable')}}" as value 
+    from {{ ref('stg_variable') }}
+
+)
+select * from variables
